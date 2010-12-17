beefd1d096b8fa80e53a946a0364dd6a465d38c5
cliksmodernos <cliksmodernos@583dbb3d-2d3b-a68a-b4b7-e9a2417a4aa8>
2010-12-17 19:31:43 +0000

    Comments :P

---  jedi-se2010/JEDI-API/src/main/java/api/JEDIGame.java
+++  jedi-se2010/JEDI-API/src/main/java/api/JEDIGame.java
@@ -4,34 +4,61 @@ import javax.swing.JPanel;
 
 public abstract class JEDIGame extends JPanel implements ButtonListenerInterface, ConnectionListenerInterface, CalibrationInterface {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * JEDI_api instances that the game will handle
+	 */
 	protected JEDI_api jedi1;
 	protected JEDI_api jedi2;
 
+	/**
+	 * Callback interface to be called at the start and ending of the game
+	 */
 	private GameCallbackInterface callback;
 	
+	/**
+	 * Constructor of a Game with two JEDIs
+	 * 
+	 * @param jedi1 Instance of the JEDI_ONE
+	 * @param jedi2 Instance of the JEDI_TWO
+	 */
 	public JEDIGame(JEDI_api jedi1, JEDI_api jedi2) {
 		this.jedi1 = jedi1;
 		this.jedi2 = jedi2;
 	}
 	
+	/**
+	 * Constructor of a Game with just one JEDI
+	 * 
+	 * @param jedi1 Instance of the JEDI_ONE
+	 */
 	public JEDIGame(JEDI_api jedi1) {
 		this.jedi1 = jedi1;
 	}
 
+	/**
+	 * Run the game with the current callback function
+	 * 
+	 * @param callback interface to be exectuted at the start and ending of the game
+	 */
 	public void run(GameCallbackInterface callback){
 		callback.onStart();
 		start();
 		this.callback = callback;
 	}
 	
+	/**
+	 * Gets the callback reference
+	 * 
+	 * @return gets the callback reference
+	 */
 	public GameCallbackInterface getCallback(){
 		return callback;
 	}
 	
+	/**
+	 * Method to be implemented by the caller of the function
+	 */
 	protected abstract void start();
 }
