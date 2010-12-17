eb9fdc2dece86469e7c6509449003d39b0e576bd
cliksmodernos <cliksmodernos@583dbb3d-2d3b-a68a-b4b7-e9a2417a4aa8>
2010-12-17 19:31:43 +0000

    Comments :P

--- trunk/ jedi-se2010/JEDI-API/src/main/java/api/GameCallbackInterface.java
+++ trunk/ jedi-se2010/JEDI-API/src/main/java/api/GameCallbackInterface.java
@@ -1,7 +1,14 @@
 package api;
 
 public interface GameCallbackInterface {
+	
+	/**
+	 * Method to be called before the start of a game 
+	 */
 	public void onStart();
 	
+	/**
+	 * Method to be called after the game finished
+	 */
 	public void onFinish();
 }
